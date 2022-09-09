d4582bec0b79d949b485554e79e3395d3e2daba2
Atul PS <96699083+Atul-PS@users.noreply.github.com>
2022-09-09 16:57:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import test
 inp = int(input())
 if inp==1:
   if 1==1:
-    return True
+    print(1)
 if inp==2:
   if 1==1:
-    return True
+    print(2)
 if inp==3:
   if 1==2:
-    return True
+    print(3)
